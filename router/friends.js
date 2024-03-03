fdd94c564ecef6a411dceb484a39c7d18d302ce1
[Ayman Shiham] <sharktank38000@gmail.com>
2024-03-03 18:46:24 -0500

    implemented GET Request to retrieve all friends

--- router/friends.js
+++ router/friends.js
@@ -12,9 +12,7 @@ let friends = {
 // GET request: Retrieve all friends
 router.get("/",(req,res)=>{
 
-  // Update the code here
-
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify({friends},null,4))//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
